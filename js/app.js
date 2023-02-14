b92cd7ab9e214eed34284b6596bfebb94a2ddb1d
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-14 15:52:48 +0100

    FEAT array with object created

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// console.log('Test');
+
+
+// Creare l’array di oggetti
+
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const dipendenti = [
+	{
+		firstName: 'Wayne',
+		lastName: 'Barnett',
+        role: 'Founder & CEO',
+		img: 'wayne-barnett-founder-ceo.jpg'
+	},
+    {
+		firstName: 'Angela',
+		lastName: 'Caroll',
+        role: 'Chief Editor',
+		img: 'angela-caroll-chief-editor.jpg'
+	},
+    {
+		firstName: 'Walter',
+		lastName: 'Gordon',
+        role: 'Office Manager',
+		img: 'walter-gordon-office-manager.jpg'
+	},
+    {
+		firstName: 'Angela',
+		lastName: 'Lopez',
+        role: 'Social Media Manager',
+		img: 'angela-lopez-social-media-manager.jpg'
+	},
+    {
+		firstName: 'Scott',
+		lastName: 'Estrada',
+        role: 'Developer',
+		img: 'scott-estrada-developer.jpg'
+	},
+    {
+		firstName: 'Barbara',
+		lastName: 'Ramos',
+        role: 'Graphic Designer',
+		img: 'barbara-ramos-graphic-designer.jpg'
+	},
+]
+
+console.log(dipendenti)
